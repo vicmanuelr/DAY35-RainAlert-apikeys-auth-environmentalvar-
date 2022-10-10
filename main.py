4f51bab92b3424a6646da449801139f53b2af663
Victor Ponce <vicmanuelr@gmail.com>
2022-10-10 15:09:31 -0600

    Imported twilio package to project
    - copied from the quickstart guide of the twilio API the instructions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-import datetime as dt
+from twilio.rest import Client
+import os
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "20e48b5ebf465fc69d4befd150b459db"
@@ -11,13 +12,29 @@ PARAMETERS = {
     "appid": API_KEY,
 }
 
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
+
+
 
 def get_data():
     """Will use parameters and endpoint to check Openweathermap API return list"""
     response = requests.get(OWM_ENDPOINT, params=PARAMETERS)
     response.raise_for_status()
     raw_data = response.json()
-    return raw_data["list"][0:4]
+    return raw_data["list"][:4]  # this equals 4 elements of 3 hours forecast each, total 12 hours
 
 
 def rain_check(list_data_elements):
